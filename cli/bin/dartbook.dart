b5f572f182a7fa8e3bee8be6d59b5530fe9dfe5b
lindeer <le.chang118@gmail.com>
2022-08-04 10:56:15 +0800

    Integrate command parser

--- cli/bin/dartbook.dart
+++ cli/bin/dartbook.dart
@@ -1,4 +1,8 @@
-import  'package:dartbook/context.dart';
+import 'dart:async' show FutureOr;
+import 'dart:io' show Platform, exit;
+
+import 'package:args/command_runner.dart';
+import 'package:dartbook/context.dart';
 import 'package:dartbook/output.dart';
 import 'package:path/path.dart' as p;
 import 'package:shelf_static/shelf_static.dart' show createStaticHandler;
@@ -6,44 +10,126 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'package:watcher/watcher.dart' show DirectoryWatcher;
 
 void main(List<String> args) {
-  final options = {
-    'format': 'website',
-    'log': 'debug',
-  };
+  final runner = CommandRunner<int>('dartbook', "A dart implementation of gitbook");
+  final parser = runner.argParser;
+  parser.addFlag('verbose', abbr: 'v', help: 'Show additional diagnostic info');
 
-  int len = args.length;
-  final root = len > 0 ? args[0] : '.';
-  final out = len > 1 ? args[1] : '_book';
+  runner.addCommand(_BuildCommand());
+  runner.addCommand(_ServeCommand());
+  runner.run(args).catchError((error) {
+    if (error is! UsageException) throw error;
+    print(error);
+    exit(64); // Exit code 64 indicates a usage error.
+  });
+}
 
-  final rootDir = p.absolute(root);
+abstract class _MainCommand extends Command<int> {
 
-  final context = BookContext.assemble(
-    root: rootDir,
-    options: options,
-  );
+  _MainCommand() {
+    argParser.addOption('format', abbr: 'f', allowed: ['ebook', 'website'], defaultsTo: 'website',
+        help: 'what kind of output to generate');
+  }
 
-  final fmt = options['format'] ?? 'website';
-  final outDir = p.join(rootDir, out);
-  final output = Output.generate(context, Option(
-    format: fmt,
-    root: outDir,
-  ));
+  @override
+  String get invocation => '${runner!.executableName} $name [<options>] [root] [output]';
 
-  print('Watch files at $rootDir ...');
-  DirectoryWatcher(p.absolute(rootDir)).events.listen((e) {
-    if (p.isWithin(outDir, e.path)) {
-      return;
+  @override
+  FutureOr<int> run() {
+    final result = argResults;
+    final rest = result?.rest ?? [];
+    String root = '.';
+    String out = '_book';
+    if (rest.isEmpty) {
+      root = p.normalize(p.absolute(root));
+      out = p.normalize(p.absolute(out));
+    } else if (rest.length == 1) {
+      root = p.normalize(p.absolute(rest[0]));
+      out = p.join(root, out);
+    } else {
+      root = p.normalize(p.absolute(rest[0]));
+      out = p.normalize(p.absolute(rest[1]));
     }
-    output.onFileChanged(e);
-  });
 
-  print('\nStarting server ...');
-  final port = int.tryParse(options['port'] ?? '') ?? 4000;
-  io.serve(
-    createStaticHandler(outDir, defaultDocument: 'index.html'),
-    'localhost',
-    port,
-  ).then((server) {
-    print('Serving book on http://localhost:4000');
-  });
+    final global = globalResults;
+    String? v;
+    final option = <String, String>{
+      for (final opt in (global?.options ?? <String>[]))
+        if ((v = global?[opt]?.toString()) != null)
+          opt: v!,
+      for (final opt in (result?.options ?? <String>[]))
+        if ((v = result?[opt]?.toString()) != null)
+          opt: v!,
+    };
+    _makeOutput(root, out, option);
+    return 0;
+  }
+
+  Output _makeOutput(String rootDir, String outDir, Map<String, String> option) {
+    final context = BookContext.assemble(
+      root: rootDir,
+      options: option,
+    );
+    final fmt = option['format'] ?? 'website';
+    final output = Output.generate(context, Option(
+      format: fmt,
+      root: outDir,
+    ));
+    return output;
+  }
+}
+
+class _BuildCommand extends _MainCommand {
+
+  @override
+  final name = 'build';
+
+  @override
+  final description = 'Build a book in specified directory';
+}
+
+class _ServeCommand extends _MainCommand {
+  @override
+  final name = 'serve';
+
+  @override
+  final description = 'Serve the book as a website for testing';
+
+  _ServeCommand() {
+    argParser.addOption('port', abbr: 'p', help: 'Port for server to listen on');
+    argParser.addFlag('watch', abbr: 'w', defaultsTo: true,
+      help: 'Enable file watcher and live reloading',);
+  }
+
+  @override
+  Output _makeOutput(String rootDir, String outDir, Map<String, String> option) {
+    final output = super._makeOutput(rootDir, outDir, option);
+    final logger = output.context.logger;
+    if (option['watch'] == "true") {
+      print('Watch files at $rootDir ...');
+      DirectoryWatcher(p.absolute(rootDir)).events.listen((e) {
+        if (p.isWithin(outDir, e.path)) {
+          return;
+        }
+        output.onFileChanged(e);
+      }).onError((e) {
+        final solution = Platform.isLinux ? """
+sudo sysctl fs.inotify.max_user_watches=1048576
+or
+adding 'fs.inotify.max_user_watches=1048576' to '/etc/sysctl.conf'"""
+            : '$e';
+        logger.i("Watch directory failed:\n$solution");
+      });
+    }
+
+    print('\nStarting server ...');
+    final port = int.tryParse(option['port'] ?? '') ?? 4000;
+    io.serve(
+      createStaticHandler(outDir, defaultDocument: 'index.html'),
+      'localhost',
+      port,
+    ).then((server) {
+      print('Serving book on http://localhost:$port');
+    });
+    return output;
+  }
 }
