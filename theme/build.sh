29fbfe4d0b30d8824e649963b4e8a469627b28cd
lindeer <le.chang118@gmail.com>
2022-12-18 20:22:45 +0800

    theme deployment

--- theme/build.sh
+++ theme/build.sh
@@ -10,3 +10,6 @@ fi
 cp build/main.dart.js resource/_assets/website/dartbook.js
 cp build/materialize.js resource/_assets/website/materialize.js
 cp build/styles.css resource/_assets/website/style.css
+
+rm -rf ../cli/theme/_assets/website && cp -r resource/_assets/website ../cli/theme/_assets
+cp resource/_layouts/website/*.html ../cli/theme/_layouts/website
