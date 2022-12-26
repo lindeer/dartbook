81350ddfbd262ed2df8aac720704494ee64f1dcd
lindeer <le.chang118@gmail.com>
2022-12-26 16:08:11 +0800

    support ignore rules

--- models/lib/ignore.dart
+++ models/lib/ignore.dart
@@ -1,12 +1,13 @@
+import 'package:glob/glob.dart' show Glob;
 
 class BookIgnore {
-  final _rules = <String>[];
+  final _rules = <Glob>[];
 
-  Iterable<String> get rules => _rules;
+  Iterable<String> get rules => _rules.map((e) => e.pattern);
 
   bool isIgnored(String filename) {
     for (final r in _rules) {
-      if (r.contains(filename)) {
+      if (r.matches(filename)) {
         return true;
       }
     }
@@ -14,11 +15,11 @@ class BookIgnore {
   }
 
   void add(String rule) {
-    _rules.add(rule);
+    _rules.add(Glob(rule));
   }
 
   void addAll(Iterable<String> rules) {
-    _rules.addAll(rules);
+    _rules.addAll(rules.map((e) => Glob(e)));
   }
 
   void update(BookIgnore ignore) {
